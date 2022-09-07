370ffd30a33567afc50e1e34ba7fde92268f8738
karen derbyshire <karenderbyshire5@gmail.com>
2022-09-07 10:01:14 +0100

    route to api endpoint changed

--- client/app.js
+++ client/app.js
@@ -4,6 +4,7 @@ const getFlights = () => {
   fetch("http://localhost:8000/flights")
     .then((response) => response.json())
     .then((flights) => {
+      // console.log(flights)
       populateTable(flights);
     })
     .catch((err) => console.log(err));
@@ -18,6 +19,30 @@ const populateTable = (flights) => {
     const tableIcon = document.createElement("td");
     tableIcon.textContent = "+";
     tableRow.append(tableIcon);
+
+    const flightDetails = {
+      time: flight.departing.slice(0, 5),
+      destination: flight.destination.toUppercase(),
+      flight: flight.flightNumber.shift(),
+      gate: flight.gate,
+      remarks: flight.status.toUppercase(),
+    };
+
+    for (const flightDetail in flightDetails) {
+      const tableCell = document.createElement("td");
+      const word = Array.from(flightDetails[flightDetail]);
+      console.log(flightDetail)
+
+      for (const [index, letter] of word.entries()) {
+        const letterElement = document.createElement("div");
+
+        letterElement.textContent = letter;
+
+        tableCell.append(letterElement);
+      }
+      tableRow.append(tableCell);
+    }
+
     tableBody.append(tableRow);
   }
 };
--- server.js
+++ server.js
@@ -7,9 +7,11 @@ require("dotenv").config();
 app.use(cors());
 
 app.get("/flights", (req, res) => {
+  const axios = require("axios");
+
   const options = {
     method: "GET",
-    url: "https://toronto-pearson-airport.p.rapidapi.com/arrivals/carousel/9",
+    url: "https://toronto-pearson-airport.p.rapidapi.com/departures",
     headers: {
       "X-RapidAPI-Key": process.env.RAPID_API_KEY,
       "X-RapidAPI-Host": "toronto-pearson-airport.p.rapidapi.com",
@@ -26,5 +28,4 @@ app.get("/flights", (req, res) => {
       console.error(error);
     });
 });
-
 app.listen(PORT, () => console.log("running on PORT " + PORT));
